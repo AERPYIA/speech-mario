93b613d867df39435fb18ec1fcc3deb73c352f34
Aerpyia <92689246+AERPYIA@users.noreply.github.com>
2022-11-10 21:16:06 -0300

    hehe agora a pontuação deu certo

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe"); 
 const moeda = document.querySelector("#moeda"); 
 
-
+//voz
 const webkitSpeechRecognition = document.querySelector(".speechApi")
 const textarea = document.querySelector("#textarea")
 const btnGravar = document.querySelector("#btnGravar")
 const btnParar = document.querySelector("#btnParar")
-
 const btnGravarmodal = document.querySelector("#btnGravarmodal")
 const btnPararmodal = document.querySelector("#btnPararmodal")
 const textareamodal = document.querySelector("#textareamodal")
-
 const btnGravargover = document.querySelector("#btnGravargover")
 const btnParargover = document.querySelector("#btnParargover")
 
@@ -24,8 +22,8 @@ const btnReiniciar = document.querySelectorAll('#btnReiniciar');
 const btnRanking = document.querySelector('#btnRanking');
 const cenario = document.querySelector('#cenario');
 const pontuacao = document.querySelector('#pontuacao');
+const tempo = document.querySelector('#tempo');
 
-let points = 0
 
 const jump = () => {
   mario.classList.add("jump");
@@ -59,7 +57,7 @@ const validaJogador = ({ target }) => {
         // Desabilita o botão
         btnStart.setAttribute('disabled', '');
     }
-}; // Chamada da função;
+}; 
 inputJogador.addEventListener('input', validaJogador);
 
 const limpaTexto = () => {
@@ -71,15 +69,26 @@ const nomevoz = () =>{
 
   inputJogador.value = textarea.value.replace(' ', '')
 }
-
+var cronometro
+let points = 0
 const loopDoJogo = setInterval(() => {
   const pipePosition = pipe.offsetLeft; 
   const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", ""); 
-
-
+  const moedaPosition = +window.getComputedStyle(moeda).bottom.replace("px", ""); 
   pipe.classList.add('desabilitar')
   moeda.classList.add('desabilitar')
+  console.log(moedaPosition)
 
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition > 80 && moedaPosition <=103){
+    moeda.style.bottom = "572px"
+    points++
+    pontuacao.innerHTML = points
+
+    setTimeout(() => {
+      moeda.style.bottom = "102px"
+    }, 1000);
+  }
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
     pipe.style.animation = "none";
@@ -92,51 +101,39 @@ const loopDoJogo = setInterval(() => {
     mario.style.width = "80px";
     mario.style.marginLeft = "50px";
 
+    moeda.style.animation ='none'
+    moeda.classList.remove('active')
+
     clearInterval(loopDoJogo); 
-    moeda.classList.add('desabilitar')
-    
+    clearInterval(cronometro)
 
-    modal.classList.remove('desabilitar'); // Habilita o modal e o fundo preto;
-    modalGameOver.classList.add('active'); // Habilita a tela de game-over no modal;
-
+    modal.classList.remove('desabilitar'); 
+    modalGameOver.classList.add('active'); 
   }
-}, 10);
+});
 
 const comecar = () => {
-  // Oculta o modal e a tela escura;
-
   modal.classList.add('desabilitar');
-  // Oculta a tela de inicio;
   modalStart.classList.remove('active');
-
-  limpaTexto();
-
-  
   pipe.classList.add('active')
   moeda.classList.add('active')
+  limpaTexto();
+  textarea.value = ""
 
+  var value = parseFloat(tempo.innerHTML)
 
-
-
+  cronometro = setInterval(() => {
+    value ++
+    
+    tempo.innerHTML = value
+  }, 1000);
 };
-
 const reiniciar = () => {
   location.reload();
 };
 btnReiniciar.forEach((btn) => {
   btn.addEventListener('click', reiniciar);
 });
-
-document.addEventListener('keydown', jump);    
-
-
-
-
-
-
-
-
-
 
 
 // VVVVVVVVVVVVVVVVVVVVVVVVVVVVVOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
@@ -171,6 +168,7 @@ class speechApi {
       if(transcript.match(/nome/)){
         nomevoz()
         stop()
+        btnStart.removeAttribute('disabled')
       }
     }
   }
@@ -183,26 +181,22 @@ class speechApi {
     this.speechApi.stop()
   }
 }
-  var speech = new speechApi()
-
-
-  btnGravar.addEventListener("click", e => {
+var speech = new speechApi()
+btnGravar.addEventListener("click", e => {
     btnGravar.disabled = true
     btnParar.disabled = false
     speech.start()
     btnGravargover.disabled = true
     btnParargover.disabled = false
-  })
-  btnParar.addEventListener("click", () => {
+})
+btnParar.addEventListener("click", () => {
     btnGravar.disabled = false
     btnParar.disabled = true
     speech.stop()
     btnGravargover.disabled = false
     btnParargover.disabled = true
-  })
-
-
-  btnGravarmodal.addEventListener("click", e => {
+})
+btnGravarmodal.addEventListener("click", e => {
     btnGravarmodal.disabled = true
     btnPararmodal.disabled = false
     speech.start()
@@ -210,9 +204,8 @@ class speechApi {
     btnParar.disabled = false
     btnGravargover.disabled = true
     btnParargover.disabled = false
-  })
-
-  btnPararmodal.addEventListener("click", () => {
+})
+btnPararmodal.addEventListener("click", () => {
     btnGravarmodal.disabled = false
     btnPararmodal.disabled = true
     speech.stop()
@@ -220,20 +213,18 @@ class speechApi {
     btnGravar.disabled = false
     btnGravargover.disabled = false
     btnParargover.disabled = true
-  })
-
-  btnGravargover.addEventListener("click", e => {
+})
+btnGravargover.addEventListener("click", e => {
     btnGravargover.disabled = true
     btnParargover.disabled = false
     speech.start()
     btnGravar.disabled = true
     btnParar.disabled = false
-  })
-
-  btnParargover.addEventListener("click", () => {
+})
+btnParargover.addEventListener("click", () => {
     btnGravargover.disabled = false
     btnParargover.disabled = true
     speech.stop()
     btnParar.disabled= true
     btnGravar.disabled = false
-  });
+});
